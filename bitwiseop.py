73ee8e360069aef22f3f168005ad13d3769be8bc
Juan Zapata <juan.zapata@upct.es>
2021-04-02 14:34:20 +0200

    añadidas las del proyecto
    Signed-off-by: Juan Zapata <juan.zapata@upct.es>

--- bitwiseop.py
+++ bitwiseop.py
@@ -1,8 +1,12 @@
 import time
+import timeit
+
 import cv2
 
 #cv2.useOptimized()
 # Load two images
+from pandas import np
+
 img1 = cv2.imread('images/messi5.jpg')
 
 # cv2.getTickCount function returns the number of clock-cycles after a
@@ -27,41 +31,40 @@ for i in range(5,49,2):
 e2 = time.time()
 t = (e2 - e1)
 print(t)
-#
-#
-# # Python scalar operations are faster than Numpy scalar operations. So for operations including one or two elements, Python scalar is better than Numpy arrays. Numpy takes advantage when size of array is a little bit bigger.
-#
-# start_time = timeit.default_timer()
-# x = 5; y = x**2
-# print "OpenCV x**2",(timeit.default_timer() - start_time)
-#
-# start_time = timeit.default_timer()
-# x = 5; y = x*x
-# print "OpenCV x*x",(timeit.default_timer() - start_time)
-#
-# start_time = timeit.default_timer()
-# x = np.uint8([5]); y = x*x
-# print "Mix Numpy x*x",(timeit.default_timer() - start_time)
-#
-# start_time = timeit.default_timer()
-# x = np.uint8([5]);y = np.square(x)
-# print "Numpy square",(timeit.default_timer() - start_time)
-#
-# # Normally, OpenCV functions are faster than Numpy functions. So for same operation, OpenCV functions are preferred. But, there can be exceptions, especially when Numpy works with views instead of copies.
-#
-# img1 = cv2.imread('/Users/juanzapata/opencv/samples/data/messi5.jpg',0)
-# start_time = timeit.default_timer()
-# z = cv2.countNonZero(img1) #only for single channel array
-# print "OpenCV",(timeit.default_timer() - start_time)
-#
-# start_time = timeit.default_timer()
-# z = np.count_nonzero(img1)
-# print "Numpy",(timeit.default_timer() - start_time)
-#
-#
-# # cv2.imshow('res',img1)
-# # #     #cv2.imshow() to display an image in a window. The window automatically fits to the image size.
-# # cv2.waitKey(0)
-# # #     #cv2.waitKey() is a keyboard binding function. Its argument is the time in milliseconds. The function waits for specified milliseconds for any keyboard event. If you press any key in that time, the program continues. If 0 is passed, it waits indefinitely for a key stroke. It can also be set to detect specific key strokes like, if key a is pressed etc which we will discuss below.
-# # cv2.destroyAllWindows()
-# # #     #cv2.destroyAllWindows() simply destroys all the windows we created. If you want to destroy any specific window, use the function cv2.destroyWindow() where you pass the exact window name as the argument.
+
+
+# Python scalar operations are faster than Numpy scalar operations. So for operations including one or two elements, Python scalar is better than Numpy arrays. Numpy takes advantage when size of array is a little bit bigger.
+
+start_time = timeit.default_timer()
+x = 5; y = x**2
+print("OpenCV x**2", (timeit.default_timer() - start_time))
+
+start_time = timeit.default_timer()
+x = 5; y = x*x
+print("OpenCV x*x", (timeit.default_timer() - start_time))
+
+start_time = timeit.default_timer()
+x = np.uint8([5]); y = x*x
+print("Mix Numpy x*x", (timeit.default_timer() - start_time))
+
+start_time = timeit.default_timer()
+x = np.uint8([5]);y = np.square(x)
+print("Numpy square", (timeit.default_timer() - start_time))
+
+# Normally, OpenCV functions are faster than Numpy functions. So for same operation, OpenCV functions are preferred. But, there can be exceptions, especially when Numpy works with views instead of copies.
+
+img1 = cv2.imread('/Users/juanzapata/opencv/samples/data/messi5.jpg',0)
+start_time = timeit.default_timer()
+z = cv2.countNonZero(img1) #only for single channel array
+print("OpenCV", (timeit.default_timer() - start_time))
+
+start_time = timeit.default_timer()
+z = np.count_nonzero(img1)
+print("Numpy", (timeit.default_timer() - start_time))
+
+# cv2.imshow('res',img1)
+# #     #cv2.imshow() to display an image in a window. The window automatically fits to the image size.
+# cv2.waitKey(0)
+# #     #cv2.waitKey() is a keyboard binding function. Its argument is the time in milliseconds. The function waits for specified milliseconds for any keyboard event. If you press any key in that time, the program continues. If 0 is passed, it waits indefinitely for a key stroke. It can also be set to detect specific key strokes like, if key a is pressed etc which we will discuss below.
+# cv2.destroyAllWindows()
+# #     #cv2.destroyAllWindows() simply destroys all the windows we created. If you want to destroy any specific window, use the function cv2.destroyWindow() where you pass the exact window name as the argument.
