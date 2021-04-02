ff3aa755f3f7f5bbbb97fddffacca0966e51b18c
Juan Zapata <juan.zapata@upct.es>
2021-04-02 13:53:26 +0200

    añadidas las del proyecto
    Signed-off-by: Juan Zapata <juan.zapata@upct.es>

--- adaptativethreshold.py
+++ adaptativethreshold.py
@@ -11,17 +11,18 @@ img = cv2.medianBlur(img, 5)
 ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 # plt.imshow(img,cmap='gray')
 
-# In this, the algorithm calculate the threshold for a small regions of the image. 
-# So we get different thresholds for different regions of the same image and it gives 
-# us better results for images with varying illumination.
+# In this, the algorithm calculate the threshold for a small regions of the
+# image. So we get different thresholds for different regions of the same
+# image and it gives us better results for images with varying illumination.
 # Adaptive Method - It decides how thresholding value is calculated.
-#         cv.ADAPTIVE_THRESH_MEAN_C : threshold value is the mean of neighbourhood area.
-#         cv.ADAPTIVE_THRESH_GAUSSIAN_C : threshold value is the weighted sum of neighbourhood 
-#            values where weights are a gaussian window.
-# Block Size - It decides the size of neighbourhood area.
+# cv.ADAPTIVE_THRESH_MEAN_C : threshold value is the mean of neighbourhood
+# area. cv.ADAPTIVE_THRESH_GAUSSIAN_C : threshold value is the weighted sum
+# of neighbourhood values where weights are a gaussian window. Block Size -
+# It decides the size of neighbourhood area.
 
 
-# C - It is just a constant which is subtracted from the mean or weighted mean calculated.
+# C - It is just a constant which is subtracted from the mean or weighted
+# mean calculated.
 
 th2 = cv2.adaptiveThreshold(
     img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
--- bitwiseop.py
+++ bitwiseop.py
@@ -1,28 +1,32 @@
-import numpy as np
-import cv2
 import time
-import timeit
-
+import cv2
 
 #cv2.useOptimized()
 # Load two images
-img1 = cv2.imread('/Users/juanzapata/opencv/samples/data/messi5.jpg')
-e1 = cv2.getTickCount() #cv2.getTickCount function returns the number of clock-cycles after a reference event (like the moment machine was switched ON) to the moment this function is called.
+img1 = cv2.imread('images/messi5.jpg')
+
+# cv2.getTickCount function returns the number of clock-cycles after a
+# reference event (like the moment machine was switched ON) to the moment
+# this function is called.
+e1 = cv2.getTickCount()
 for i in range(5,49,2):
     img1 = cv2.medianBlur(img1,i)
-e2 = cv2.getTickCount() #cv2.getTickFrequency function returns the frequency of clock-cycles, or the number of clock-cycles per second.
+e2 = cv2.getTickCount()
+
+# cv2.getTickFrequency function returns the frequency of clock-cycles,
+# or the number of clock-cycles per second.
 t = (e2 - e1)/cv2.getTickFrequency()
-print (t)
+print(t)
 # # Result I got is 0.521107655 seconds
 
 # Load two images
-img1 = cv2.imread('/Users/juanzapata/opencv/samples/data/messi5.jpg')
+img1 = cv2.imread('images/messi5.jpg')
 e1 = time.time()
 for i in range(5,49,2):
     img1 = cv2.medianBlur(img1,i)
 e2 = time.time()
 t = (e2 - e1)
-print (t)
+print(t)
 #
 #
 # # Python scalar operations are faster than Numpy scalar operations. So for operations including one or two elements, Python scalar is better than Numpy arrays. Numpy takes advantage when size of array is a little bit bigger.
