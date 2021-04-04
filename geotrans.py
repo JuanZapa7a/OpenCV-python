4b9ae8fe2887a51c26d10c0fff11ca48cc4552f5
Juan Zapata <juan.zapata@upct.es>
2021-04-04 14:55:41 +0200

    solucion a cv2.imshow es plt.imshow plt.show
    Signed-off-by: Juan Zapata <juan.zapata@upct.es>

--- geotrans.py
+++ geotrans.py
@@ -1,35 +1,46 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+img = cv2.imread('images/messi5.jpg')
 
-img = cv2.imread('/Users/juanzapata/opencv/samples/data/messi5.jpg')
-
-
-cv2.imshow('original',img)
+plt.imshow(img,cmap = 'gray')
+plt.show()
+#cv2.imshow('original',img)
 
 # The size of the image can be specified manually, or you can specify the scaling factor. Different interpolation methods are used. Preferable interpolation methods are cv2.INTER_AREA for shrinking and cv2.INTER_CUBIC (slow) & cv2.INTER_LINEAR for zooming. By default, interpolation method used is cv2.INTER_LINEAR for all resizing purposes. You can resize an input image either of following methods:
 
 
 res = cv2.resize(img,None,fx=2,fy=2, interpolation = cv2.INTER_CUBIC)
-cv2.imshow('transf1',res)
+plt.imshow(res,cmap = 'gray')
+plt.show()
+#cv2.imshow('transf1',res)
 
 #OR
 
 height, width = img.shape[:2]
 res2 = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+plt.imshow(res2,cmap = 'gray')
+plt.show()
+#cv2.imshow('Transf2',res2)
 
-cv2.imshow('Transf2',res2)
-
-img = cv2.imread('/Users/juanzapata/opencv/samples/data/messi5.jpg',0) #only one chanel
+img = cv2.imread('images/messi5.jpg',0)
+#only one chanel
 rows,cols = img.shape
-M = np.float32([[1,0,100],[0,1,50]]) # Transmatrix M=([1 0 tx],[0 1 ty])
+M = np.float32([[1,0,100],[0,1,50]])
+# Transmatrix M=([1 0 tx],[0 1 ty])
 
 dst = cv2.warpAffine(img,M,(cols,rows))
-cv2.imshow('Translated Image ',dst)
+plt.imshow(dst,cmap = 'gray')
+plt.show()
+#cv2.imshow('Translated Image ',dst)
 
 
-M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)# Transmatrix M=([alpha betha (1-alpha)cx-betha*cy],[-betha alpha betha*cx+(1-alpha)*cy])
+M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+# Transmatrix M=([alpha betha (1-alpha)cx-betha*cy],[-betha alpha betha*cx+(1-alpha)*cy])
 dst = cv2.warpAffine(img,M,(cols,rows))
-cv2.imshow('Rotated Image ',dst)
+plt.imshow(dst,cmap = 'gray')
+plt.show()
+#cv2.imshow('Rotated Image ',dst)
 
     #cv2.imshow() to display an image in a window. The window automatically fits to the image size.
 cv2.waitKey(0)
